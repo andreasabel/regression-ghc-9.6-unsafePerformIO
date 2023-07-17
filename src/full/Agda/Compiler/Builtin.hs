526c78f0ff7f9cc17c76edc169d1aa1fd6bd38f3
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-07-17 16:21:15 +0200

    Remove JS backend

--- src/full/Agda/Compiler/Builtin.hs
+++ src/full/Agda/Compiler/Builtin.hs
@@ -7,7 +7,6 @@ module Agda.Compiler.Builtin where
 import Agda.Compiler.Backend (Backend)
 
 import Agda.Compiler.MAlonzo.Compiler (ghcBackend)
-import Agda.Compiler.JS.Compiler (jsBackend)
 import Agda.Interaction.Highlighting.Dot (dotBackend)
 import Agda.Interaction.Highlighting.HTML (htmlBackend)
 import Agda.Interaction.Highlighting.LaTeX (latexBackend)
@@ -15,7 +14,6 @@ import Agda.Interaction.Highlighting.LaTeX (latexBackend)
 builtinBackends :: [Backend]
 builtinBackends =
   [ ghcBackend
-  , jsBackend
   , dotBackend
   , htmlBackend
   , latexBackend
