bdaa63cd918f30b21e29906212d996eacdeed78e
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-07-17 16:23:18 +0200

    Remove MAlonzo backend

--- src/full/Agda/Compiler/Builtin.hs
+++ src/full/Agda/Compiler/Builtin.hs
@@ -6,15 +6,13 @@ module Agda.Compiler.Builtin where
 
 import Agda.Compiler.Backend (Backend)
 
-import Agda.Compiler.MAlonzo.Compiler (ghcBackend)
 import Agda.Interaction.Highlighting.Dot (dotBackend)
 import Agda.Interaction.Highlighting.HTML (htmlBackend)
 import Agda.Interaction.Highlighting.LaTeX (latexBackend)
 
 builtinBackends :: [Backend]
 builtinBackends =
-  [ ghcBackend
-  , dotBackend
+  [ dotBackend
   , htmlBackend
   , latexBackend
   ]
