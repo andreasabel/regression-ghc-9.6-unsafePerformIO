6fe1fa7e15bd0f3328f92eeadbb5846fcaf18887
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-07-17 21:08:59 +0200

    Interaction.Imports: remove getStoredInterface

--- src/full/Agda/Interaction/Imports.hs
+++ src/full/Agda/Interaction/Imports.hs
@@ -68,7 +68,6 @@ import qualified Agda.Interaction.Options.Lenses as Lens
 import Agda.Interaction.Options.Warnings (unsolvedWarnings)
 
 import Agda.Utils.FileName
-import Agda.Utils.Lens
 import Agda.Utils.Maybe
 import qualified Agda.Utils.Maybe.Strict as Strict
 import Agda.Utils.Monad
@@ -78,16 +77,6 @@ import Agda.Utils.Hash
 import qualified Agda.Utils.Trie as Trie
 
 import Agda.Utils.Impossible
-
--- | Whether to ignore interfaces (@.agdai@) other than built-in modules
-
-ignoreInterfaces :: HasOptions m => m Bool
-ignoreInterfaces = optIgnoreInterfaces <$> commandLineOptions
-
--- | Whether to ignore all interface files (@.agdai@)
-
-ignoreAllInterfaces :: HasOptions m => m Bool
-ignoreAllInterfaces = optIgnoreAllInterfaces <$> commandLineOptions
 
 -- | The decorated source code.
 
@@ -483,31 +472,11 @@ getInterface x isMain msrc =
           let file = srcOrigin src
           modifyTCLens stModuleToSource $ Map.insert x (srcFilePath file)
           pure file
-      reportSLn "import.iface" 15 $ List.intercalate "\n" $ map ("  " ++)
-        [ "module: " ++ prettyShow x
-        , "file:   " ++ prettyShow file
-        ]
 
-      reportSLn "import.iface" 10 $ "  Check for cycle"
-      checkForImportCycle
-
-      -- -- Andreas, 2014-10-20 AIM XX:
-      -- -- Always retype-check the main file to get the iInsideScope
-      -- -- which is no longer serialized.
-      -- let maySkip = isMain == NotMainInterface
-      -- Andreas, 2015-07-13: Serialize iInsideScope again.
-      -- Andreas, 2020-05-13 issue #4647: don't skip if reload because of top-level command
-      stored <- runExceptT $ Bench.billTo [Bench.Import] $ do
-        getStoredInterface x file msrc
-
-      let recheck = \reason -> do
-            reportSLn "import.iface" 5 $ concat ["  ", prettyShow x, " is not up-to-date because ", reason, "."]
-            setCommandLineOptions . stPersistentOptions . stPersistentState =<< getTC
-            case isMain of
-              MainInterface _ -> createInterface x file isMain msrc
-              NotMainInterface -> createInterfaceIsolated x file msrc
-
-      either recheck pure stored
+      setCommandLineOptions . stPersistentOptions . stPersistentState =<< getTC
+      case isMain of
+        MainInterface _ -> createInterface x file isMain msrc
+        NotMainInterface -> createInterfaceIsolated x file msrc
 
 -- | Check if the options used for checking an imported module are
 --   compatible with the current options. Raises Non-fatal errors if
@@ -545,126 +514,6 @@ getOptionsCompatibilityWarnings isMain isPrim currentOptions i = runMaybeT $ exc
                   (iTopLevelModuleName i)) $
     throwError "No warnings to collect because options were compatible"
   lift $ getAllWarnings' isMain ErrorWarnings
-
--- | Try to get the interface from interface file or cache.
-
-getStoredInterface
-  :: TopLevelModuleName
-     -- ^ Module name of file we process.
-  -> SourceFile
-     -- ^ File we process.
-  -> Maybe Source
-  -> ExceptT String TCM ModuleInfo
-getStoredInterface x file msrc = do
-  -- Check whether interface file exists and is in cache
-  --  in the correct version (as testified by the interface file hash).
-  --
-  -- This is a lazy action which may be skipped if there is no cached interface
-  -- and we're ignoring interface files for some reason.
-  let getIFileHashesET = do
-        -- Check that the interface file exists and return its hash.
-        ifile <- maybeToExceptT "the interface file could not be found" $ MaybeT $
-          findInterfaceFile' file
-
-        -- Check that the interface file exists and return its hash.
-        hashes <- maybeToExceptT "the interface file hash could not be read" $ MaybeT $ liftIO $
-          getInterfaceFileHashes ifile
-
-        return (ifile, hashes)
-
-  -- Examine the hash of the interface file. If it is different from the
-  -- stored version (in stDecodedModules), or if there is no stored version,
-  -- read and decode it. Otherwise use the stored version.
-  --
-  -- This is a lazy action which may be skipped if the cached or on-disk interface
-  -- is invalid, missing, or skipped for some other reason.
-  let checkSourceHashET ifaceH = do
-        sourceH <- case msrc of
-                    Nothing -> liftIO $ hashTextFile (srcFilePath file)
-                    Just src -> return $ hashText (srcText src)
-
-        unless (sourceH == ifaceH) $
-          throwError $ concat
-            [ "the source hash (", show sourceH, ")"
-            , " does not match the source hash for the interface (", show ifaceH, ")"
-            ]
-
-        reportSLn "import.iface" 5 $ concat ["  ", prettyShow x, " is up-to-date."]
-
-  -- Check if we have cached the module.
-  cachedE <- runExceptT $ maybeToExceptT "the interface has not been decoded" $ MaybeT $
-      lift $ getDecodedModule x
-
-  case cachedE of
-    -- If it's cached ignoreInterfaces has no effect;
-    -- to avoid typechecking a file more than once.
-    Right mi -> do
-      (ifile, hashes) <- getIFileHashesET
-
-      let ifp = filePath $ intFilePath ifile
-      let i = miInterface mi
-
-      -- Make sure the hashes match.
-      let cachedIfaceHash = iFullHash i
-      let fileIfaceHash = snd hashes
-      unless (cachedIfaceHash == fileIfaceHash) $ do
-        lift $ dropDecodedModule x
-        reportSLn "import.iface" 50 $ "  cached hash = " ++ show cachedIfaceHash
-        reportSLn "import.iface" 50 $ "  stored hash = " ++ show fileIfaceHash
-        reportSLn "import.iface" 5 $ "  file is newer, re-reading " ++ ifp
-        throwError $ concat
-          [ "the cached interface hash (", show cachedIfaceHash, ")"
-          , " does not match interface file (", show fileIfaceHash, ")"
-          ]
-
-      Bench.billTo [Bench.Deserialization] $ do
-        checkSourceHashET (iSourceHash i)
-
-        reportSLn "import.iface" 5 $ "  using stored version of " ++ (filePath $ intFilePath ifile)
-        loadDecodedModule file mi
-
-    Left whyNotCached -> withExceptT (\e -> concat [whyNotCached, " and ", e]) $ do
-      whenM ignoreAllInterfaces $
-        throwError "we're ignoring all interface files"
-
-      whenM ignoreInterfaces $
-        unlessM (lift $ Lens.isBuiltinModule (filePath $ srcFilePath file)) $
-            throwError "we're ignoring non-builtin interface files"
-
-      (ifile, hashes) <- getIFileHashesET
-
-      let ifp = (filePath . intFilePath $ ifile)
-
-      Bench.billTo [Bench.Deserialization] $ do
-        checkSourceHashET (fst hashes)
-
-        reportSLn "import.iface" 5 $ "  no stored version, reading " ++ ifp
-
-        i <- maybeToExceptT "bad interface, re-type checking" $ MaybeT $
-          readInterface ifile
-
-        -- Ensure that the given module name matches the one in the file.
-        let topLevelName = iTopLevelModuleName i
-        unless (topLevelName == x) $
-          -- Andreas, 2014-03-27 This check is now done in the scope checker.
-          -- checkModuleName topLevelName file
-          lift $ typeError $ OverlappingProjects (srcFilePath file) topLevelName x
-
-        isPrimitiveModule <- lift $ Lens.isPrimitiveModule (filePath $ srcFilePath file)
-
-        lift $ chaseMsg "Loading " x $ Just ifp
-        -- print imported warnings
-        let ws = filter ((Strict.Just (Just x) ==) .
-                         fmap rangeFileName . tcWarningOrigin) $
-                 iWarnings i
-        unless (null ws) $ reportSDoc "warning" 1 $ P.vcat $ P.prettyTCM <$> ws
-
-        loadDecodedModule file $ ModuleInfo
-          { miInterface = i
-          , miWarnings = []
-          , miPrimitive = isPrimitiveModule
-          , miMode = ModuleTypeChecked
-          }
 
 
 loadDecodedModule
@@ -835,11 +684,6 @@ highlightFromInterface
   -> TCM ()
 highlightFromInterface i file = return ()
 
--- | Read interface file corresponding to a module.
-
-readInterface :: InterfaceFile -> TCM (Maybe Interface)
-readInterface file = return Nothing
-
 -- | Tries to type check a module and write out its interface. The
 -- function only writes out an interface file if it does not encounter
 -- any warnings.
@@ -920,15 +764,6 @@ createInterface mname file isMain msrc = do
 getAllWarnings' :: (MonadFail m, ReadTCState m, MonadWarning m, MonadTCM m) => MainInterface -> WhichWarnings -> m [TCWarning]
 getAllWarnings' (MainInterface _) = getAllWarningsPreserving unsolvedWarnings
 getAllWarnings' NotMainInterface  = getAllWarningsPreserving Set.empty
-
--- Andreas, issue 964: not checking null interactionPoints
--- anymore; we want to serialize with open interaction points now!
-
--- | Returns (iSourceHash, iFullHash)
---   We do not need to check that the file exist because we only
---   accept @InterfaceFile@ as an input and not arbitrary @AbsolutePath@!
-getInterfaceFileHashes :: InterfaceFile -> IO (Maybe (Hash, Hash))
-getInterfaceFileHashes _ = return Nothing
 
 moduleHash :: TopLevelModuleName -> TCM Hash
 moduleHash m = iFullHash <$> getNonMainInterface m Nothing
