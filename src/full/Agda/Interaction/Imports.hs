9709d6cfc60ae8de5f5ef55caacdf15f0ad8a2f0
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-07-18 16:17:03 +0200

    Imports: shrink away getInterface

--- src/full/Agda/Interaction/Imports.hs
+++ src/full/Agda/Interaction/Imports.hs
@@ -13,28 +13,20 @@ module Agda.Interaction.Imports
   , typeCheckMain
   ) where
 
-import Prelude hiding (null)
-
-import qualified Data.Map as Map
 import qualified Data.Text.Lazy as TL
 
 import qualified Agda.Syntax.Concrete as C
-import Agda.Syntax.Concrete.Attribute
-import Agda.Syntax.Common
-import Agda.Syntax.Parser
-import Agda.Syntax.Position
-import Agda.Syntax.TopLevelModuleName
-import Agda.Syntax.Translation.ConcreteToAbstract as CToA
+import Agda.Syntax.Common (FileType)
+import Agda.Syntax.Parser (moduleParser, parseFile, readFilePM)
+import Agda.Syntax.Position (mkRangeFile)
+import Agda.Syntax.TopLevelModuleName (TopLevelModuleName)
+import Agda.Syntax.Translation.ConcreteToAbstract (concreteToAbstract_, TopLevel(..))
 
-import Agda.TypeChecking.Warnings hiding (warnings)
-import Agda.TypeChecking.Monad
-import qualified Agda.TypeChecking.Monad.Benchmark as Bench
+import Agda.TypeChecking.Warnings (runPM)
+import Agda.TypeChecking.Monad (TCM, envCurrentPath, localTC, setOptionsFromPragma)
 
-import Agda.Interaction.FindFile
+import Agda.Interaction.FindFile (SourceFile(..), moduleName)
 import Agda.Interaction.Library.Base (OptionsPragma(..))
-import Agda.Interaction.Options
-
-type AgdaLibFile = ()
 
 -- | The decorated source code.
 
@@ -44,34 +36,29 @@ data Source = Source
   , srcOrigin      :: SourceFile            -- ^ Source location at the time of its parsing
   , srcModule      :: C.Module              -- ^ The parsed module.
   , srcModuleName  :: TopLevelModuleName    -- ^ The top-level module name.
-  , srcProjectLibs :: [AgdaLibFile]         -- ^ The .agda-lib file(s) of the project this file belongs to.
-  , srcAttributes  :: !Attributes
-    -- ^ Every encountered attribute.
   }
 
 -- | Parses a source file and prepares the 'Source' record.
 
 parseSource :: SourceFile -> TCM Source
-parseSource sourceFile@(SourceFile f) = Bench.billTo [Bench.Parsing] $ do
-  (source, fileType, parsedMod, attrs, parsedModName) <- mdo
+parseSource sourceFile@(SourceFile f) = do
+  (source, fileType, parsedMod, parsedModName) <- mdo
     -- This piece of code uses mdo because the top-level module name
     -- (parsedModName) is obtained from the parser's result, but it is
     -- also used by the parser.
     let rf = mkRangeFile f (Just parsedModName)
     source                         <- runPM $ readFilePM rf
-    ((parsedMod, attrs), fileType) <- runPM $
+    ((parsedMod, _attrs), fileType) <- runPM $
                                       parseFile moduleParser rf $
                                       TL.unpack source
     parsedModName                  <- moduleName f parsedMod
-    return (source, fileType, parsedMod, attrs, parsedModName)
+    return (source, fileType, parsedMod, parsedModName)
   return Source
     { srcText        = source
     , srcFileType    = fileType
     , srcOrigin      = sourceFile
     , srcModule      = parsedMod
     , srcModuleName  = parsedModName
-    , srcProjectLibs = []
-    , srcAttributes  = attrs
     }
 
 srcFilePragmas :: Source -> [OptionsPragma]
@@ -85,84 +72,21 @@ srcFilePragmas src = pragmas
             | C.OptionsPragma r opts <- cpragmas
             ]
 
--- | Set options from a 'Source' pragma, using the source
---   ranges of the pragmas for error reporting.
-setOptionsFromSourcePragmas :: Source -> TCM ()
-setOptionsFromSourcePragmas src = do
-  mapM_ setOptionsFromPragma (srcFilePragmas    src)
-
--- | Is the aim to type-check the top-level module, or only to
--- scope-check it?
-
-data Mode
-  = ScopeCheck
-  | TypeCheck
-  deriving (Eq, Show)
-
-
--- | Type checks the main file of the interaction.
---   This could be the file loaded in the interacting editor (emacs),
---   or the file passed on the command line.
---
---   First, the primitive modules are imported.
---   Then, @getInterface@ is called to do the main work.
---
---   If the 'Mode' is 'ScopeCheck', then type-checking is not
---   performed, only scope-checking. (This may include type-checking
---   of imported modules.) In this case the generated, partial
---   interface is not stored in the state ('stDecodedModules'). Note,
---   however, that if the file has already been type-checked, then a
---   complete interface is returned.
 
 typeCheckMain
   :: Source
      -- ^ The decorated source code.
   -> TCM ()
 typeCheckMain src = do
-  -- For the main interface, we also remember the pragmas from the file
-  setOptionsFromSourcePragmas src
+  let srcPath = srcFilePath $ srcOrigin src
 
-  getInterface (srcModuleName src) src
+  localTC (\e -> e { envCurrentPath = Just srcPath }) $ do
 
-
-getInterface
-  :: TopLevelModuleName
-  -> Source
-  -> TCM ()
-getInterface x src = do
-
-     let file = srcOrigin src
-     modifyTCLens stModuleToSource $ Map.insert x (srcFilePath file)
-
-     createInterface x file src
-
-
--- | Tries to type check a module and write out its interface. The
--- function only writes out an interface file if it does not encounter
--- any warnings.
---
--- If appropriate this function writes out syntax highlighting
--- information.
-
-createInterface
-  :: TopLevelModuleName    -- ^ The expected module name.
-  -> SourceFile            -- ^ The file to type check.
-  -> Source
-  -> TCM ()
-createInterface mname file src = do
-  Bench.billTo [Bench.TopModule mname] $ do
-    localTC (\e -> e { envCurrentPath = Just (srcFilePath file) }) $ do
-
-    let srcPath = srcFilePath $ srcOrigin src
-
-    setOptionsFromSourcePragmas src
-    checkAttributes (srcAttributes src)
-    syntactic <- optSyntacticEquality <$> pragmaOptions
-    localTC (\env -> env { envSyntacticEqualityFuel = syntactic }) $ do
+    mapM_ setOptionsFromPragma (srcFilePragmas src)
 
     -- Scope checking.
-    _topLevel <- Bench.billTo [Bench.Scoping] $ do
+    _topLevel <- do
       let topDecls = C.modDecls $ srcModule src
-      concreteToAbstract_ (TopLevel srcPath mname topDecls)
+      concreteToAbstract_ (TopLevel srcPath (srcModuleName src) topDecls)
 
     return ()
