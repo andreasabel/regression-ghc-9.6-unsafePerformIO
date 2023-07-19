1a955d2a158ae84eb21e579c0ff5493eaaba76fe
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-07-19 08:00:05 +0200

    Shrink ImpossibleTest

--- src/full/Agda/ImpossibleTest.hs
+++ src/full/Agda/ImpossibleTest.hs
@@ -3,20 +3,10 @@
 module Agda.ImpossibleTest where
 
 import Agda.TypeChecking.Monad.Base   ( TCM, ReduceM, runReduceM )
-import Agda.TypeChecking.Monad.Debug  ( MonadDebug, __IMPOSSIBLE_VERBOSE__ )
-import Agda.TypeChecking.Reduce.Monad ()
+import Agda.TypeChecking.Monad.Debug  ( __IMPOSSIBLE_VERBOSE__ )
+import Agda.TypeChecking.Reduce.Monad () -- instance MonadDebug ReduceM
 
-import Agda.Utils.CallStack           ( HasCallStack )
-import Agda.Utils.Impossible          ( __IMPOSSIBLE__ )
-
--- | If the given list of words is non-empty, print them as debug message
---   (using '__IMPOSSIBLE_VERBOSE__') before raising the internal error.
-impossibleTest :: (MonadDebug m, HasCallStack) => [String] -> m a
-impossibleTest = \case
-  []   -> __IMPOSSIBLE__
-  strs -> __IMPOSSIBLE_VERBOSE__ $ unwords strs
+import GHC.Stack                      ( HasCallStack )
 
 impossibleTestReduceM :: (HasCallStack) => [String] -> TCM a
-impossibleTestReduceM = runReduceM . \case
-  []   -> __IMPOSSIBLE__
-  strs -> __IMPOSSIBLE_VERBOSE__ $ unwords strs
+impossibleTestReduceM = runReduceM . __IMPOSSIBLE_VERBOSE__ . unwords
